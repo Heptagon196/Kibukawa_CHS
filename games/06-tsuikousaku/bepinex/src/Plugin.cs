c704f06dd189cf6d1b8fa18bfc275bb99612c908
heptagon196 <heptagon196@outlook.com>
2026-09-11 00:44:13 +0800

    fix(kibu6): remove incorrect gender from absence messages

--- games/06-tsuikousaku/bepinex/src/Plugin.cs
+++ games/06-tsuikousaku/bepinex/src/Plugin.cs
@@ -13,7 +13,7 @@ using UnityEngine.UI;
 
 namespace Kibu1ZhCN
 {
-    [BepInPlugin(Id, "Kibu6 Simplified Chinese", "1.0.6")]
+    [BepInPlugin(Id, "Kibu6 Simplified Chinese", "1.0.7")]
     [BepInProcess("kibu6.exe")]
     public sealed class Plugin : BaseUnityPlugin
     {
